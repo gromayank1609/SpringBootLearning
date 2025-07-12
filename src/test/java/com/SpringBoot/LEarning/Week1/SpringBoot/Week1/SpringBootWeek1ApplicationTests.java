13051ef7874be6b58f40d9ac5618f8939f79a2e2
gromayank1609 <gromayank1609@gmail.com>
2025-07-12 15:04:29 +0530

    intial commit

---
+++ src/test/java/com/SpringBoot/LEarning/Week1/SpringBoot/Week1/SpringBootWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.LEarning.Week1.SpringBoot.Week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
